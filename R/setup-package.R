11bf7e58b8145c50ce1449d641506db07a8bd4e6
David Keyes <dgkeyes@gmail.com>
2019-03-05 14:15:31 -0800

    Testing

---
+++ R/setup-package.R
@@ -0,0 +1,19 @@
+library(tidyverse)
+library(here)
+
+# This file brings in assets (data, setup.Rmd and CSS) from the assets repo (https://github.com/rfortherestofus/assets).
+
+# Get Data ----------------------------------------------------------------
+
+download.file("https://github.com/rfortherestofus/assets/raw/master/data/nhanes.csv",
+              destfile = here("data", "nhanes.csv"))
+
+# Get setup.Rmd -----------------------------------------------------------
+
+download.file("https://github.com/rfortherestofus/course-assets/raw/master/misc/setup.Rmd",
+              destfile = here("slides", "setup.Rmd"))
+
+# Get CSS -----------------------------------------------------------------
+
+download.file("https://raw.githubusercontent.com/rfortherestofus/course-assets/master/style/style.css",
+              destfile = here("slides", "style.css"))
