8cd69f35b0270e47ea25227e459915a90dbe23b0
David Keyes <dgkeyes@gmail.com>
2019-04-12 11:36:46 -0700

    Updates

--- R/setup-package.R
+++ R/setup-package.R
@@ -8,6 +8,9 @@ library(here)
 download.file("https://github.com/rfortherestofus/assets/raw/master/data/nhanes.csv",
               destfile = here("data", "nhanes.csv"))
 
+download.file("https://github.com/rfortherestofus/assets/raw/master/data/nhanes-codebook.csv",
+              destfile = here("data", "nhanes-codebook.csv"))
+
 # Get setup.Rmd -----------------------------------------------------------
 
 download.file("https://github.com/rfortherestofus/course-assets/raw/master/misc/setup.Rmd",
